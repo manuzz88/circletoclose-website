77f45111426546f419e532bd3191de54c7d9e3d1
Manuel Lazzaro <manuel.lazzaro@italfrigo.com>
2025-08-29 09:12:25 +0200

    Clean: Removed all complex components for simple landing page deploy

--- src/components/providers/ClientProviders.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import React from 'react';
-import { AuthProvider } from '../auth/AuthProvider';
-
-export default function ClientProviders({ children }: { children: React.ReactNode }) {
-  return <AuthProvider>{children}</AuthProvider>;
-}
